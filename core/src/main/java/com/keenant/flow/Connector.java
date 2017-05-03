658de07b5c128222b28990002bede24f43dd4d6a
thekeenant <keenan@keenant.com>
2017-05-03 01:10:46 -0500

    Add functional connector method

--- core/src/main/java/com/keenant/flow/Connector.java
+++ core/src/main/java/com/keenant/flow/Connector.java
@@ -3,6 +3,8 @@ package com.keenant.flow;
 import com.keenant.flow.exception.DatabaseException;
 
 import java.sql.Connection;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 /**
  * Provides an opened database connection.
@@ -15,6 +17,34 @@ import java.sql.Connection;
  * </ul>
  */
 public interface Connector {
+    /**
+     * Construct an anonymous connector. This is handy in Java 8 when implementing interfaces that
+     * have multiple methods, since you can now use functional programming!
+     *
+     * @param acquire supplies a connection
+     * @param dispose disposes a connection
+     * @param disposeAll disposes all connections
+     * @return the connector
+     */
+    static Connector of(Supplier<Connection> acquire, Consumer<Connection> dispose, Runnable disposeAll) {
+        return new Connector() {
+            @Override
+            public Connection acquire() throws DatabaseException {
+                return acquire.get();
+            }
+
+            @Override
+            public void dispose(Connection connection) {
+                dispose.accept(connection);
+            }
+
+            @Override
+            public void disposeAll() {
+                disposeAll.run();
+            }
+        };
+    }
+
     /**
      * Open a new database connection.
      * @return a new connection
